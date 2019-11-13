8661aa9c773e236c7ed889ab354456e93ef39180
AlexRzh <a.rzhevtsev@netrika.ru>
2019-11-13 13:09:11 +0300

    automatic drawing

--- src/Loader.java
+++ src/Loader.java
@@ -1,20 +1,37 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Loader {
-    public static void main(String[] args) {
-        String[][] arrayX = {
-                {"X", " ", " ", " ", " ", " ", "X"},
-                {" ", "X", " ", " ", " ", "X", " "},
-                {" ", " ", "X", " ", "X", " ", " "},
-                {" ", " ", " ", "X", " ", " ", " "},
-                {" ", " ", "X", " ", "X", " ", " "},
-                {" ", "X", " ", " ", " ", "X", " "},
-                {"X", " ", " ", " ", " ", " ", "X"}
-        };
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int c = Integer.parseInt(reader.readLine());
+
+        char[][] arrayX = new char[c][c];
+
+        int middle = c / 2;
 
         for(int i = 0; i < arrayX.length; i++){
             for(int j = 0; j < arrayX[i].length; j++){
-                System.out.print(arrayX[i][j]);
+                if(i == j) {
+                    arrayX[i][j] = 'X';
+                }
+                else if(Math.abs(i - middle) == Math.abs(j - middle)){
+                    arrayX[i][j] = 'X';
+                }
+                else{
+                    arrayX[i][j] = ' ';
+                }
+            }
+        }
+
+        for(char[] x : arrayX){
+            for(char y : x){
+                System.out.print(y);
             }
             System.out.println();
         }
+
+        reader.close();
     }
 }
